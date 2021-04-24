d9baeaa0fb5ae9f595f9ca62c473f920e06e2377
Abhishek Tanwer <abhishektanwer2@gmail.com>
2021-04-24 14:05:03 -0400

    added component for addcalories functionality

--- src/app/dailycalories/dailycalories.component.spec.ts
+++ src/app/dailycalories/dailycalories.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UpdateprofileComponent } from './updateprofile.component';
+import { DailycaloriesComponent } from './dailycalories.component';
 
-describe('UpdateprofileComponent', () => {
-  let component: UpdateprofileComponent;
-  let fixture: ComponentFixture<UpdateprofileComponent>;
+describe('DailycaloriesComponent', () => {
+  let component: DailycaloriesComponent;
+  let fixture: ComponentFixture<DailycaloriesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpdateprofileComponent ]
+      declarations: [ DailycaloriesComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UpdateprofileComponent);
+    fixture = TestBed.createComponent(DailycaloriesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
